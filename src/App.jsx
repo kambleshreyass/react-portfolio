7f5b45fafebd19d2222e7ec23eee926416d2f534
Shreyas Kamble <kshreyass59@gmail.com>
2024-07-25 22:47:00 +0530

    This is day 02 of this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Header from "./components/Header"
 
 
 const App = () => {
   return (
-    <div className="App"></div>
+    <>
+      <Header/>
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+
+
+const Header = () => {
+  return (
+    <nav>
+      <NavContent/>
+    </nav>
+  )
+}
+
+const NavContent = () => (
+  <>
+    <h1>Shreyass.</h1>
+    <div>
+      <a href="#home">Home</a>
+      <a href="#projects">Projects</a>
+      <a href="#resume">Resume</a>
+      <a href="#skills">Skills</a>
+      <a href="#contact">Contact</a>
+    </div>
+    <a href="mailto:kshreyass59@gamail.com">
+      <button>Email</button>
+    </a>
+  </>
+)
+
+export default Header
